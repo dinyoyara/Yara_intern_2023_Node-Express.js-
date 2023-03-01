695220cfd1b30b9e658f63146b16726c7c49604e
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-01 17:57:25 +0200

    Add password in User model.

--- models/user.js
+++ models/user.js
@@ -1,30 +1,48 @@
 const { DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 const sequelize = require('../db');
 
-const User = sequelize.define('user', {
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true
+const User = sequelize.define(
+    'user',
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        role: {
+            type: DataTypes.ENUM('admin`', 'write', 'read'),
+            allowNull: false
+        },
+        deleted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
     },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
-    role: {
-        type: DataTypes.ENUM('admin`', 'write', 'read'),
-        allowNull: false
-    },
-    deleted: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
+    {
+        instanceMethods: {
+            generateHash(password) {
+                return bcrypt.hash(password, bcrypt.genSaltSync(8));
+            },
+            validPassword(password) {
+                return bcrypt.compare(password, this.password);
+            }
+        }
     }
-});
+);
 
 module.exports = User;
