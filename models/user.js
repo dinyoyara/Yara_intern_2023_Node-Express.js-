672a40f9e0005737eb084b3dd4d2df548c93d0c4
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-02 15:05:41 +0200

    Fix User model methods.

--- models/user.js
+++ models/user.js
@@ -3,46 +3,36 @@ const bcrypt = require('bcrypt');
 
 const sequelize = require('../db');
 
-const User = sequelize.define(
-    'user',
-    {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        role: {
-            type: DataTypes.ENUM('admin`', 'write', 'read'),
-            defaultValue: 'read'
-        },
-        deleted: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        }
+const User = sequelize.define('user', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
     },
-    {
-        instanceMethods: {
-            generateHash(password) {
-                return bcrypt.hash(password, bcrypt.genSaltSync(8));
-            },
-            validPassword(password) {
-                return bcrypt.compare(password, this.password);
-            }
-        }
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.ENUM('admin', 'write', 'read'),
+        defaultValue: 'read'
+    },
+    deleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
     }
-);
+});
+
+User.prototype.generateHash = async (password) => await bcrypt.hash(password, bcrypt.genSaltSync(8));
+User.prototype.validPassword = async (password) => await bcrypt.compare(password, this.password);
 
 module.exports = User;
