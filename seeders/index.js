26f5dfae716aea906016e7843f938715d490a354
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-02 15:07:03 +0200

    Add user seeder.

--- seeders/index.js
+++ seeders/index.js
@@ -1,9 +1,11 @@
 const contractorSeeder = require('./contractor');
 const warehouseSeeder = require('./warehouse');
 const productSeeder = require('./products');
+const userSeeder = require('./user');
 
 module.exports = async () => {
     await contractorSeeder(5);
     await warehouseSeeder(3);
     await productSeeder(15);
+    await userSeeder(5);
 };
---
+++ seeders/user.js
@@ -0,0 +1,18 @@
+const User = require('../models/user');
+
+module.exports = async (count) => {
+    for (let i = 0; i < count; i++) {
+        let user = await User.build({
+            name: 'user_00' + (i + 1),
+            email: 'u1' + i + 'ser@abc.us'
+        });
+        user.password = await user.generateHash(`p${i + 1}${i + 1}${i + 1}${i + 1}`);
+        if (i == 0) {
+            user.role = 'admin';
+        }
+        if (i % 2 != 0) {
+            user.role = 'write';
+        }
+        await user.save();
+    }
+};
