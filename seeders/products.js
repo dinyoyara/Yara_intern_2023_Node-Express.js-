4658c137f8add2a3e164c3ec2176a3c870e282c6
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-02 09:12:39 +0200

    Clean code.

--- seeders/products.js
+++ seeders/products.js
@@ -34,5 +34,5 @@ module.exports = async (count) => {
 function getRandom(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min) + min);
 }
