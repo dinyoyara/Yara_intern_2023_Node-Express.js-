7035d414685cc17ac8d856d0bb1a32549604922f
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-02 17:19:54 +0200

    Add setHeader middleware.

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const router = require('./routes/main');
-const sequelize = require('./db');
-
-const { Contractor, Product, Warehouse, Invertory, Transfer, User } = require('./models');
-const seeder = require('./seeders');
+// const sequelize = require('./db');
+// const { Contractor, Product, Warehouse, Invertory, Transfer, User } = require('./models');
+// const seed = require('./seeders');
+const setHeaders = require('./middlewares/setHeader');
+const productsRoutes = require('./routes/products');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(router);
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(setHeaders);
+app.use('/products', productsRoutes);
 
-sequelize.sync().then(async () => {
-    await seeder();
-});
+// sequelize.sync().then(async () => {
+//     await seed();
+// });
 
 app.listen(3000);
