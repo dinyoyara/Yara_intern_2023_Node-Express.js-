530e90624f19ac737278d9398d9db1703b296183
Dinyo <dinyo517@gmail.com>
2023-03-05 19:09:23 +0200

    Update user seeder.

--- seeders/user.js
+++ seeders/user.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 
+const { generateCryptedPassword } = require('../helpers/bcrypt');
+
 module.exports = async (count) => {
     if ((await User.findAndCountAll()).count > 0) return;
     for (let i = 0; i < count; i++) {
@@ -7,13 +9,13 @@ module.exports = async (count) => {
             name: 'user_00' + (i + 1),
             email: 'u1' + i + 'ser@abc.us'
         });
-        user.password = await user.generateHash(`p${i + 1}${i + 1}${i + 1}${i + 1}`);
         if (i == 0) {
             user.role = 'admin';
         }
         if (i % 2 != 0) {
             user.role = 'write';
         }
+        user.password = await generateCryptedPassword(`p${i + 1}${i + 1}${i + 1}${i + 1}`);
         await user.save();
     }
 };
