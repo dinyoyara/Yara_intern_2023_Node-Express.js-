e57edeb601d883e86416c436c4dcb816021c826b
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-06 10:14:36 +0200

    Set token expiration.

--- jwt/token.js
+++ jwt/token.js
@@ -3,7 +3,7 @@ var jwt = require('jsonwebtoken');
 const config = require('./config.json');
 
 const generateToken = (data) => {
-    return jwt.sign(data, config.jwtSecret, { expiresIn: '3h' });
+    return jwt.sign(data, config.jwtSecret, { expiresIn: 10 * 60 });
 };
 
 module.exports = {
